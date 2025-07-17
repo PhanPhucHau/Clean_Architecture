eedced8ac4047d39a2670889a63ac56ac96155e6
PhanPhucHau <phanphuchau15@gmail.com>
2025-07-17 16:39:57 +0700

    connect db

---
+++ CleanArchitechtureDemo.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitechtureDemo.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitechtureDemo.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // register connect database service
+        services.AddDbContext<ApplicationDBContext>(options =>
+        options.UseNpgsql(
+            configuration.GetConnectionString("DefaultConnection"),
+            npgsql => npgsql.MigrationsAssembly(
+                typeof(ApplicationDBContext).Assembly.FullName)));
+        return services;
+    }
+}
---
+++ CleanArchitechtureDemo.Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,41 @@
+﻿using Clean_Architecture.Infrastructure.Persistence.EntityConfigurations.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace CleanArchitechtureDemo.Infrastructure.Persistence;
+
+public class ApplicationDBContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+
+  //  private readonly IEnumerable<ISaveChangesInterceptor> _interceptors;
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<ApplicationRole> ApplicationRoles { get; set; }
+    //  public DbSet<Project> Projects { get; set; }
+
+
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+    {
+
+    }
+
+    //public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    //{
+    //    return await base.SaveChangesAsync(cancellationToken);
+    //}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //foreach (var interceptor in _interceptors)
+        //{
+        //    optionsBuilder.AddInterceptors(interceptor);
+        //}
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
